a2698ac3915a03d44fd62821547e3cb3567f10d3
hcirrr <ngthuhien.385@gmail.com>
2025-01-14 13:53:23 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+import TrangChu from './components/TrangChu'
+
+function App() {
+
+  return (
+    <>
+      <TrangChu/>
+      {/* <Dashboard/> */}
+    </>
+  )
+}
+
+export default App
